09ea689d5ebf38d4836ab8e10d152792451f0010
Aiswarya0302 <ishuu0317@gmail.com>
2025-12-28 15:49:33 +0530

    Add files via upload

---
+++ MySampleFile.java
@@ -0,0 +1,22 @@
+
+
+package java_program;
+
+public class MySampleFile {
+
+	public static void main(String[] args) {
+		 
+		int mark=76;
+		
+		if(mark>40) {
+			System.out.println("Pass");
+		}
+		else {
+			System.out.println("Fail");
+		}
+		
+
+	}
+
+}
+
