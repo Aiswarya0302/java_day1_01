7681750c1a57f7af7c148331202277d8ab59097b
Aiswarya0302 <ishuu0317@gmail.com>
2025-12-28 16:28:46 +0530

    Add files via upload

---
+++ assingment02for.java
@@ -0,0 +1,37 @@
+package assingment02;
+
+public class assingment02for {
+
+	public static void main(String[] args) {
+		
+		//using for loop
+		
+	     for(int i = 0 ; i < 20 ; i++) {
+	    	 
+	    	 if(i % 2 == 0 ) {
+	    		 
+	    		 System.out.println(i+":" +"This is a Even num");
+	    	 }
+	    	 
+	    	 else {
+	    		 
+	    		 System.out.println(i + ":" +"This is Odd num");
+	    		 
+	    	 }
+	    	 
+	     }
+	     
+	     //using if condition
+	     
+	     int num=4;
+	     
+	     if(num % 2 ==0) {
+	    	 System.out.println("This is a Even num");
+	     }
+	     else {
+	    	 System.out.print("This is a Even num");
+	     }
+
+	}
+
+}
